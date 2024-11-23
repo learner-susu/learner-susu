2711ff16c8b63f544cf5cf0466a065a076ad33d6
learner-susu <91686558+learner-susu@users.noreply.github.com>
2024-11-23 13:29:07 +0545

    Add files via upload

---
+++ subjects.php
@@ -0,0 +1,145 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BBS Third Subjects</title>
+</head>
+<body>
+    <style>
+
+    .subject1{
+        color: green;
+        font-size: 20px;
+    }
+    .subject2{
+        color: darkcyan;
+        font-size: 20px;
+    }
+    .subject3{
+        color: darkolivegreen;
+        font-size: 20px;
+    }
+    .subject4{
+        color: darkslateblue;
+        font-size: 20px;
+    }
+    .subject5{
+        color: dark;
+        font-size: 20px;
+    }
+
+    </style>
+
+</body>
+</html>
+
+
+		
+<h1>BBS 3nd Year Subjects &  Syllabus  </h1>
+
+<div class="subject1">
+    
+   <li> * Business Law  (MGT 204)
+ 
+        <ul><li>Introduction to Law and Business Law</li></ul>
+        <ul><li>The Law of Contract and Other Liablities</li></ul>
+        <ul><li>Contract of Bailment and Pledge</li></ul>
+        <ul><li>Contract of sales of Goods</li></ul>
+        <ul><li>Contact of Agency</li></ul>
+        <ul><li>Contact of Carriage</li></ul>
+        <ul><li>Contact of Indemnity and Guarantee</li></ul>
+        <ul><li>Law of Company and Isnolvency</li></ul>
+        <ul><li>Dispute Settlement Systems</li></ul>
+        <ul><li>Law of Intellecutal Property</li></ul>
+
+   </li>
+</div>
+
+<div class="subject2">
+
+        <li> * Fundamental of Marketing (MGT 214)
+            <ul><li>Introduction </li></ul>
+            <ul><li>Marketing Environment Analysis</li></ul>
+            <ul><li>Managing of Market Information</li></ul>
+            <ul><li>Understanding Bayer's Behaviour</li></ul>
+            <ul><li>Market Segmentation, Targeting Differentiation and Positioning</li></ul>
+            <ul><li>Inflation and Business cycles</li></ul>
+            <ul><li>Product, Service and Brands</li></ul>
+            <ul><li>Pricing</li></ul>
+            <ul><li>Marketing Channels</li></ul>
+            <ul><li>Promotion Decision</li></ul>
+           
+        </li>
+
+</div>
+
+<div class="subject3">
+        <ol> * Business Environment and Strategy (MGT 217)
+            <li>Part-1: Business Environment 
+            <ul><li>Introduction to Business Environment</li></ul>
+            <ul><li>Political Environment</li></ul>
+            <ul><li>Environment Regulations / Legal Environment</li></ul>
+            <ul><li>Economic Environment</li></ul>
+            <ul><li>Socio-Cultural Environment</li></ul>
+            <ul><li>Technological Environment</li></ul>
+            <ul><li>Asian and Global Environment</li></ul>
+            </li>
+
+            <li>Part-2: Strategic Management
+                <ul><li>Introduction to Strategic Management</li></ul>
+                <ul><li>External Environment and Industry Analysis</li></ul>
+                <ul><li>Organizational Behaviour</li></ul>
+                <ul><li>Strategy Formulation</li></ul>
+                <ul><li>Strategy Implementation</li></ul>
+                <ul><li>Strategy Evaluaton and Control</li></ul>
+            </li>
+        </ol>
+        
+ </div>
+
+ <div class="subject4">
+
+<ol> * Taxation in Nepal (MGT 224)
+    <li>Part-1: Organizational Behaviours
+          <ul><li>Introduction</li></ul>
+          <ul><li>Perception and Learning</li></ul>
+          <ul><li>Personality</li></ul>
+          <ul><li>Job Satisfaction</li></ul>
+          <ul><li>Groups and Teams in organizations</li></ul>
+          <ul><li>Organizational conflict and stress</li></ul>
+          <ul><li>Organizational Change and Development</li></ul>
+    </li>
+
+    <li>Part-2: Human Resource Management
+          <ul><li>Introduction</li></ul>
+          <ul><li>Job Design and Analysis</li></ul>
+          <ul><li>Talent acquistions and Development</li></ul>
+          <ul><li>Performance appraisal and Reward management</li></ul>
+          <ul><li>Employee Maintenance and Emerging issues in HRM</li></ul>
+    </li>
+
+</ol>
+
+</div>
+
+<div class="subject5">
+
+        <li> * Foundation of Financial Systems in Nepal (MGT 226)
+            <ul><li>Introduction </li></ul>
+            <ul><li>Financial Instruments and Interest Rate</li></ul>
+            <ul><li>Depository Financial Institutions</li></ul>
+            <ul><li>Non-Depository Financial Instrutions</li></ul>
+            <ul><li>Ancillary Financial Services</li></ul>
+            <ul><li>Primary Market Operation</li></ul>
+            <ul><li>Secondary Market Operation</li></ul>
+            <ul><li>Financial Regulation and Regulatory Authority</li></ul>
+            <ul><li>Exchange Rate, Foreign Revenue and Balance of Payment</li></ul>
+            
+           
+        </li>
+
+</div>
+        
